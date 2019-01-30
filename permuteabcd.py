6bf380238d584bbfb34b4d86c8d4faebf5460c94
dimitarpg13 <dimitar_pg13@hotmail.com>
2019-01-30 05:18:36 -0500

    permute abcd

---
+++ permuteabcd.py
@@ -0,0 +1,8 @@
+for first in 'ABCD':
+    for second in 'ABCD':
+        if second != first:
+            for third in 'ABCD':
+                if third != first and third != second:
+                    for fourth in 'ABCD':
+                        if fourth != first and fourth != second and fourth != third:
+                            print(first + second + third + fourth)
