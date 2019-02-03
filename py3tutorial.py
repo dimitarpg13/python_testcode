5070858c47582cac2618a3a5c712fbe5baa16bce
dimitarpg13 <dimitar_pg13@hotmail.com>
2019-02-03 15:53:43 -0500

    strings test 1

--- py3tutorial.py
+++ py3tutorial.py
@@ -1,2 +1,15 @@
 def write_multiple_items(file, separator, *args):
     file.write(separator.join(args))
+
+def concat(*args, sep="/"):
+    return sep.join(args)
+
+
+def strings_test1():
+    a = str(b'Zoot!')
+    print('a = str(b\'Zoot!\')')
+    print('a={0}'.format(a))
+    b = '01\t012\t0123\t01234'
+    print('b = \'01\\t012\\t0123\\t01234\'')
+    print('b={0}'.format(b))
+    print('b.expandtabs()={0}', b.expandtabs());
