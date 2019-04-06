f582ac2d3695c789094e07d03fb36efb2e94159f
dimitar gueorguiev <dimitar_pg13@hotmail.com>
2019-04-06 15:54:33 +0000

    remote debugging test

---
+++ infiniteloop1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import time
+
+def your_function():
+    print("Hello, World")
+
+
+while True:
+    your_function()
+    time.sleep(10) #make function to sleep for 10 seconds
